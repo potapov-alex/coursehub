228f23d0b01713666a2c9bbba7a55a51fde4301b
potapov-alex <62658932+potapov-alex@users.noreply.github.com>
2025-01-13 23:50:22 +0300

    59 done

--- src/main/java/ru/courcehb/c12/Collection/jv59.java
+++ src/main/java/ru/courcehb/c12/Collection/jv59.java
@@ -1,11 +1,18 @@
 package ru.courcehb.c12.Collection;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class jv59 {
     public static void main(String[] args) {
-        ArrayList<Integer> IntList = new ArrayList<>();
-
-
+        Set<Integer> set = new HashSet<>
+                (Arrays.stream(Arrays.stream(new Scanner(System.in).nextLine().split(","))
+                                .mapToInt(Integer::parseInt).toArray())
+                        .boxed()
+                        .collect(Collectors.toSet()));
+        System.out.println(set);
     }
 }
