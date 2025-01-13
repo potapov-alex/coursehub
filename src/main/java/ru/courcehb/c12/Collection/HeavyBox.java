613c105d4280241b80034aeee0694aa06e0bdc93
potapov-alex <62658932+potapov-alex@users.noreply.github.com>
2025-01-13 23:01:08 +0300

    HeavyBox is done

--- src/main/java/ru/courcehb/c12/Collection/HeavyBox.java
+++ src/main/java/ru/courcehb/c12/Collection/HeavyBox.java
@@ -1,21 +1,10 @@
 package ru.courcehb.c12.Collection;
 
 import java.util.ArrayList;
-import java.util.Random;
-import java.util.UUID;
 
 public class HeavyBox {
 
     private String boxName;
-    private int weight;
-
-    private int heavyBoxQuantity;
-    private ArrayList<HeavyBox> heavyBoxes;
-
-    public HeavyBox(String setBoxName, int setWeight) {
-        this.boxName = setBoxName;
-        this.weight = setWeight;
-    }
 
     public String getBoxName() {
         return boxName;
@@ -25,6 +14,8 @@ public class HeavyBox {
         this.boxName = boxName;
     }
 
+    private int weight;
+
     public int getWeight() {
         return weight;
     }
@@ -32,6 +23,8 @@ public class HeavyBox {
     public void setWeight(int weight) {
         this.weight = weight;
     }
+
+    private int heavyBoxQuantity;
 
     public int getHeavyBoxQuantity() {
         return heavyBoxQuantity;
@@ -41,26 +34,57 @@ public class HeavyBox {
         this.heavyBoxQuantity = heavyBoxQuantity;
     }
 
-    public void HeavyBoxList() {
-        this.heavyBoxes = new ArrayList<>();
-    }
-
-    public void addHeavyBox(HeavyBox heavyBox) {
-        this.heavyBoxes.add(new HeavyBox(setBoxName(UUID.randomUUID().toString()), setWeight(new Random().nextInt() * 100));
+    public HeavyBox(String setBoxName, int setWeight) {
+        this.boxName = setBoxName;
+        this.weight = setWeight;
     }
 
     @Override
     public String toString() {
-        return "Box{" +
-                "name = '" + boxName + '\'' +
-                ", weight = " + weight +
-                '}';
+        return "Box{" + "name = '" + boxName + '\'' + ", weight = " + weight + '}';
     }
 
 
     public static void main(String[] args) {
+        //ok, пошли по пунктикам
+        // 1. создать динамический массив, содержащий объекты класса HeavyBox
+        ArrayList<HeavyBox> heavyBoxes = new ArrayList<>();
 
+        // 2. распечатать его содержимое, используя for each
+        for (HeavyBox box : heavyBoxes) {
+            System.out.println(box.toString()); // не затруднительно, но бесполезно ведь там никого нет
+        }
 
+        // 3. Изменить вес первого ящика на 1
+        // а) логически невозможно, потому, что у нас нет ящиков.
+        // б) нет указаний как изменять на (+/-) 1.
+        //мы не сдаемся, для выполнения п.3 добавим объект класса HeavyBox
+        heavyBoxes.add(0, new HeavyBox("box1", 2));
+        // Создаем врем переменную для внесения изменений
+        HeavyBox tempBox = heavyBoxes.get(0);
+        // Вносим некоторые изменения
+        tempBox.setWeight(tempBox.getWeight() + 1);
+        // Возвращаем в ArrList
+        heavyBoxes.set(0, tempBox);
+
+        // 4. Удалить последний ящик (было бы невозможно если бы мы его не добавили,
+        // пришлось бы писать условия или обрабатывать Exception)
+        heavyBoxes.remove(heavyBoxes.size() - 1);
+
+        // 5. Получить массив, содержащий ящики коллекции, тремя способами и вывести на консоль.
+        //метод NumberOne
+        System.out.println("на консоль через sout");
+        System.out.println(heavyBoxes);
+        //метод NumberTwo
+        System.out.println("на консоль через for each");
+        for (HeavyBox box : heavyBoxes) {
+            System.out.println(box.toString()); // не затруднительно, но бесполезно ведь там никого нет
+        }
+        //метод NumberThree
+        //а хз в голову ничего не лезет.
+        // как и в консоль, ну а чего мы ожидали?
+
+        //6. Удалить все ящики.
+        heavyBoxes.clear();
     }
-
 }
---
+++ src/main/java/ru/courcehb/c12/Collection/jv59.java
@@ -0,0 +1,11 @@
+package ru.courcehb.c12.Collection;
+
+import java.util.ArrayList;
+
+public class jv59 {
+    public static void main(String[] args) {
+        ArrayList<Integer> IntList = new ArrayList<>();
+
+
+    }
+}
